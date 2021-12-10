e0b8b132f887f0943a1feddb864ee6dfb2d88b1d
Colin Barry <colin.andrew.barry@gmail.com>
2021-12-10 15:51:07 -0500

    Fully functional

--- src/LinkedListTest.c
+++ src/LinkedListTest.c
@@ -23,24 +23,41 @@ node_t *create_new_node(int value) {
     return result;
 }
 
+node_t *find_node(node_t *head, int value) {
+    node_t *tmp = head;
+    while (tmp != NULL) {
+        if(tmp->value == value) return tmp;
+        tmp = tmp->next;
+    }
+    return NULL;    
+}
+
+node_t *insert_at_head(node_t **head, node_t *node_to_insert) {
+    node_to_insert->next = *head;
+    *head = node_to_insert;
+    return node_to_insert;
+}
+
+void insert_after_node(node_t *node_to_insert_after, node_t *newnode) {
+    newnode->next = node_to_insert_after->next;
+    node_to_insert_after->next = newnode;
+}
+
 int main() {
-    node_t *head;
+    node_t *head = NULL;
     node_t *tmp;
 
     for (int i = 0; i < 25; ++i) {
-        
+        tmp = create_new_node(i);
+        head = insert_at_head(&head, tmp);
     }
-    tmp = create_new_node(43);
-    head = tmp;
-    tmp = create_new_node(3);
-    tmp->next = head;
-    head = tmp;
-    tmp = create_new_node(819);
-    tmp->next = head;
-    head = tmp;
+
+    tmp = find_node(head, 13);
+    printf("Found node %d!\n", tmp->value);
+
+    insert_after_node(tmp, create_new_node(75));
 
     printlist(head);
-    // printf("");
 
     return 0;
 }
